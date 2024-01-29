e21be460ae533d3300763239485135a11e6e074b
Jaliel <kangjaliel1998@gmail.com>
2024-01-29 07:20:33 +0700

    Initial Commit

---
+++ Sesi4TabViewStyle/Sesi4TabViewStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesi4TabViewStyleApp.swift
+//  Sesi4TabViewStyle
+//
+//  Created by Jaliel on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesi4TabViewStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
